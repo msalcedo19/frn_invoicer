53c59e963068a1e84ba7e7fb602da8e2fac7e83e
Mateo Salcedo Rodriguez <msalcedo@domaintools.com>
2023-03-31 11:09:03 -0500

    deploy again

--- pages/invoice/[model_id].tsx
+++ pages/invoice/[model_id].tsx
@@ -11,7 +11,7 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
-import PostInvoiceModal from "@/components/Invoice/InvoiceModal";
+import { PostInvoiceModal } from "@/components/Invoice/InvoiceModal";
 import Box from "@mui/material/Box";
 
 import { useDispatch } from "react-redux";
